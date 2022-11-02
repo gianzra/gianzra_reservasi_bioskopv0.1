2b75d6ad3de6447d8109f1c8ce001e698ea88157
gianzra <gianzra1@gmail.com>
2022-11-03 05:39:12 +0700

    Update SwaggerConfig for fix CORS

---
+++ src/main/java/com/binar/app/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.binar.app.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
+
+public class SwaggerConfig {
+
+    public OpenAPI reservasibioskopOpenApi(){
+        return new OpenAPI()
+                .info(new Info().title("gianzra_reservasi_bioskop"))
+                .servers(List.of(new Server().url("https://gianzrareservasibioskop-production.up.railway.app/ ")));
+    }
+}
