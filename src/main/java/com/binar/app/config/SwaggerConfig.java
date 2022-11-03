940afe7aac029c4bdd536d0084c65c2f44ea192e
gianzra <gianzra1@gmail.com>
2022-11-03 11:46:35 +0700

    Update SwaggerConfig for fix CORSv0.3

--- src/main/java/com/binar/app/config/SwaggerConfig.java
+++ src/main/java/com/binar/app/config/SwaggerConfig.java
@@ -3,11 +3,15 @@ package com.binar.app.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
+@Configuration
 public class SwaggerConfig {
 
+    @Bean
     public OpenAPI reservasibioskopOpenApi(){
         return new OpenAPI()
                 .info(new Info().title("gianzra_reservasi_bioskop"))
