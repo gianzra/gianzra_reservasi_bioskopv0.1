916a2d3b6f37b81d446b590ce18bf4428d22c8d7
gianzra <gianzra1@gmail.com>
2022-11-03 20:33:26 +0700

    logger update

--- src/main/java/com/binar/app/service/UserServiceImpl.java
+++ src/main/java/com/binar/app/service/UserServiceImpl.java
@@ -4,21 +4,27 @@ import com.binar.app.dto.UserDTO;
 import com.binar.app.model.User;
 import com.binar.app.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
 
+
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     UserRepository userRepository;
 
     @Override
     public User create(User user) {
+        logger.info("Menyimpan pengguna baru {} ke Database", user.getUsername());
         final User result = userRepository.save(user);
         return result;
     }
@@ -32,6 +38,8 @@ public class UserServiceImpl implements UserService {
             result.setEmail_address(user.getEmail_address());
             result.setPassword(user.getPassword());
             userRepository.save(user);
+        }else {
+            logger.error("Data pengguna tidak ditemukan");
         }
         return null;
     }
@@ -43,12 +51,15 @@ public class UserServiceImpl implements UserService {
             // hard delete
             userRepository.deleteById(id);
             return true;
+        } else {
+            logger.error("Data pengguna tidak ditemukan");
         }
         return false;
     }
 
     @Override
     public List<User> findAll() {
+        logger.info("Mengambil data pengguna dari database");
         return userRepository.findAll();
     }
 
@@ -56,7 +67,10 @@ public class UserServiceImpl implements UserService {
     public User findById(Long id) {
         Optional<User> result = userRepository.findById(id);
         if(result.isPresent()) {
+            logger.info("Mengambil data pengguna dari database");
             return result.get();
+        } else {
+            logger.error("Data pengguna tidak ditemukan");
         }
         return null;
     }
