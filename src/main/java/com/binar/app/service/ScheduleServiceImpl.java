f4193ed55b46521e9b9c437bbdd72cbc3cbe9a6c
gianzra <gianzra1@gmail.com>
2022-11-03 22:56:16 +0700

    logger SSI update v0.1

--- src/main/java/com/binar/app/service/ScheduleServiceImpl.java
+++ src/main/java/com/binar/app/service/ScheduleServiceImpl.java
@@ -4,6 +4,8 @@ import com.binar.app.dto.ScheduleDTO;
 import com.binar.app.model.Schedules;
 import com.binar.app.repository.ScheduleRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
@@ -14,6 +16,7 @@ import java.util.Optional;
 @Transactional
 public class ScheduleServiceImpl implements ScheduleService {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     ScheduleRepository scheduleRepository;
 
@@ -27,11 +30,14 @@ public class ScheduleServiceImpl implements ScheduleService {
     public Schedules update(Long schedule_id, Schedules schedule) {
         Schedules result = findById(schedule_id);
         if(result != null) {
+            logger.info("Data schedules tersedia");
             result.setTanggal_tayang(schedule.getTanggal_tayang());
             result.setJam_mulai(schedule.getJam_mulai());
             result.setJam_selesai(schedule.getJam_selesai());
             result.setHarga_tiket(schedule.getHarga_tiket());
             scheduleRepository.save(schedule);
+        }else {
+            logger.error("Data schedules tidak tersedia");
         }
         return null;
     }
@@ -40,15 +46,19 @@ public class ScheduleServiceImpl implements ScheduleService {
     public Boolean delete(Long schedule_id) {
         final Schedules result = findById(schedule_id);
         if (result != null) {
+            logger.info("Menghapus data schedules dari database");
             // hard delete
             scheduleRepository.deleteById(schedule_id);
             return true;
+        }else {
+            logger.error("Data schedules tidak tersedia");
         }
         return false;
     }
 
     @Override
     public List<Schedules> findAll() {
+        logger.info("Mengambil data schedules dari database");
         return scheduleRepository.findAll();
     }
 
@@ -56,7 +66,10 @@ public class ScheduleServiceImpl implements ScheduleService {
     public Schedules findById(Long schedule_id) {
         Optional<Schedules> result = scheduleRepository.findById(schedule_id);
         if(result.isPresent()) {
+            logger.info("Data schedules tersedia");
             return result.get();
+        }else {
+            logger.error("Data film tidak dimasukkan");
         }
         return null;
     }
