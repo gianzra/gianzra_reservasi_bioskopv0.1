db6e13d0f9efd1c935023cfb0999a2767b7492d4
gianzra <gianzra1@gmail.com>
2022-11-07 19:50:57 +0700

    Update To Challange7 Requirement1

--- src/main/java/com/binar/app/config/FirebaseConfig.java
+++ src/main/java/com/binar/app/config/FirebaseConfig.java
@@ -1,27 +1,29 @@
-//package com.binar.app.config;
-//
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.FirebaseOptions;
-//import com.google.firebase.messaging.FirebaseMessaging;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.io.ClassPathResource;
-//
-//import java.io.IOException;
-//
-//@Configuration
-//public class FirebaseConfig {
-//    @Bean
-//    FirebaseMessaging firebaseMessaging() throws IOException {
-//        GoogleCredentials googleCredentials = GoogleCredentials
-//                .fromStream(new ClassPathResource("reservasi-bioskop-e9cdd-firebase-adminsdk-g7mdo-4aafcba2d2").getInputStream());
-//        FirebaseOptions firebaseOptions = FirebaseOptions
-//                .builder()
-//                .setCredentials(googleCredentials)
-//                .build();
-//
-//        FirebaseApp firebaseApp = FirebaseApp.initializeApp(firebaseOptions, "my-app");
-//        return FirebaseMessaging.getInstance(firebaseApp);
-//    }
-//}
+package com.binar.app.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource("reservasi-bioskop-e9cdd-firebase-adminsdk-g7mdo-4aafcba2d2.json").getInputStream());
+        FirebaseOptions firebaseOptions = FirebaseOptions
+                .builder()
+                .setCredentials(googleCredentials)
+                .build();
+
+        FirebaseApp firebaseApp = FirebaseApp.initializeApp(firebaseOptions, "my-app");
+        return FirebaseMessaging.getInstance(firebaseApp);
+
+    }
+}
