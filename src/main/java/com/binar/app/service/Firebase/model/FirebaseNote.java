846eb9e8a51d7525eb66562425bc547848a96c75
gianzra <gianzra1@gmail.com>
2022-11-16 09:16:17 +0700

    Update Firebase Note

--- src/main/java/com/binar/app/service/Firebase/model/FirebaseNote.java
+++ src/main/java/com/binar/app/service/Firebase/model/FirebaseNote.java
@@ -1,15 +1,15 @@
-//package com.binar.app.service.Firebase.model;
-//
-//
-//import lombok.Data;
-//import java.util.Map;
-//
-//@Data
-//public class FirebaseNote {
-//    private String subject;
-//    private String content;
-//    private Map<String, String> data;
-//    private String image;
-//    private String token;
-//
-//}
+package com.binar.app.service.Firebase.model;
+
+
+import lombok.Data;
+import java.util.Map;
+
+@Data
+public class FirebaseNote {
+    private String subject;
+    private String content;
+    private Map<String, String> data;
+    private String image;
+    private String token;
+
+}
