0477d6ee11a699b55f95ce1454cdec41fbe01fd1
gianzra <gianzra1@gmail.com>
2022-11-16 09:15:59 +0700

    Update Firebase Messaging Service

--- src/main/java/com/binar/app/service/Firebase/FirebaseMessagingService.java
+++ src/main/java/com/binar/app/service/Firebase/FirebaseMessagingService.java
@@ -1,46 +1,43 @@
-//package com.binar.app.service.Firebase;
-//
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.firebase.messaging.FirebaseMessaging;
-//import com.google.firebase.messaging.FirebaseMessagingException;
-//import com.google.firebase.messaging.Message;
-//import com.google.firebase.messaging.Notification;
-//import lombok.extern.slf4j.Slf4j;
-//import com.binar.app.service.Firebase.model.FirebaseNote;
-//
-//import org.springframework.stereotype.Service;
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//import java.util.Arrays;
-//
-//@Service
-//@Slf4j
-//public class FirebaseMessagingService {
-//
-//    private final FirebaseMessaging firebaseMessaging;
-//
-//    public FirebaseMessagingService(FirebaseMessaging firebaseMessaging) {
-//        this.firebaseMessaging = firebaseMessaging;
-//    }
-//
-//
-//    public String sendNotification(FirebaseNote note, String topic) throws FirebaseMessagingException {
-//        Notification notification = Notification
-//                .builder()
-//                .setTitle(note.getSubject())
-//                .setBody(note.getContent())
-//                .setImage(note.getImage())
-//                .build();
-//
-//        Message message = Message
-//                .builder()
-////                .setToken(topic)
-//                .setTopic(topic)
-//                .setNotification(notification)
-//                .putAllData(note.getData())
-//                .build();
-//        log.info(firebaseMessaging.send(message));
-//        log.info("Send Firebase Messaging");
-//        return firebaseMessaging.send(message);
-//    }
-//}
+package com.binar.app.service.Firebase;
+
+import com.binar.app.service.Firebase.model.FirebaseNote;
+import com.binar.app.service.SeatServiceImpl;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FirebaseMessagingService {
+    private static final Logger logger = LoggerFactory.getLogger(FirebaseMessagingService.class);
+    @Autowired
+    private final FirebaseMessaging firebaseMessaging;
+
+    public FirebaseMessagingService(FirebaseMessaging firebaseMessaging) {
+        this.firebaseMessaging = firebaseMessaging;
+    }
+
+    public String sendNotification(FirebaseNote note, String topic) throws FirebaseMessagingException {
+        Notification notification = Notification
+                .builder()
+                .setTitle(note.getSubject())
+                .setBody(note.getContent())
+                .setImage(note.getImage())
+                .build();
+
+        Message message = Message
+                .builder()
+//                .setToken(topic)
+                .setTopic(topic)
+                .setNotification(notification)
+                .putAllData(note.getData())
+                .build();
+        logger.info(firebaseMessaging.send(message));
+        logger.info("Notification has been sent");
+        return firebaseMessaging.send(message);
+    }
+}
